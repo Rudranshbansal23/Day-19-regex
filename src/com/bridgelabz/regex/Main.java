8582f72b8ad660e01f58cb4575cdc72c8d3a9046
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-14 20:27:09 +0530

    Added:Ability to enter a valid first name

---
+++ src/com/bridgelabz/regex/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.regex;
+public class Main {
+    public static void main(String[] args) {
+        UserValid userValid = new UserValid();
+        userValid.checkFirstName();
+    }
+}
